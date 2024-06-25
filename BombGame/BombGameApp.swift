091b8a2a19ad2cf3d35d0052ea8a582a1a27346d
Marian Gaebler <marian.gaebler@gmail.com>
2024-06-25 09:40:45 -1000

    Initial Commit

---
+++ BombGame/BombGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombGameApp.swift
+//  BombGame
+//
+//  Created by Marian Gaebler on 25.06.24.
+//
+
+import SwiftUI
+
+@main
+struct BombGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
